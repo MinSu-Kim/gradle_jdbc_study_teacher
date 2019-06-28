de20bac70428b785c2929f47c92449a118adaa64
lenovo <net94.teacher@gmail.com>
2019-06-28 17:55:30 +0900

    selectDepartmentByAll()만 테스트 완료 ~~! 나머지 과제

---
+++ src/main/java/gradle_jdbc_study_teacher/daoimpl/DepartmentDaoImpl.java
@@ -0,0 +1,68 @@
+package gradle_jdbc_study_teacher.daoimpl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import gradle_jdbc_study_teacher.dao.DepartmentDao;
+import gradle_jdbc_study_teacher.dto.Department;
+import gradle_jdbc_study_teacher.jdbc.ConnectionProvider;
+
+public class DepartmentDaoImpl implements DepartmentDao {
+	static final Logger log = LogManager.getLogger();
+	
+	@Override
+	public List<Department> selectDepartmentByAll() {
+		List<Department> lists = new ArrayList<Department>();
+		String sql = "select deptno, deptname, floor from department";
+		try(Connection conn = ConnectionProvider.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement(sql);
+				ResultSet rs = pstmt.executeQuery()) {
+			log.trace(pstmt);
+
+			while(rs.next()) {
+				lists.add(getDepartment(rs));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} 
+		return lists;
+	}
+	
+	private Department getDepartment(ResultSet rs) throws SQLException {
+		return new Department(rs.getInt("deptno"), 
+				              rs.getString("deptname"), 
+				              rs.getInt("floor"));
+	}
+	
+	@Override
+	public Department selectDepartmentByNo(Department dept) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int insertDepartment(Department dept) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteDepartment(Department dept) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int updateDepartment(Department dept) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ src/test/java/gradle_jdbc_study_teacher/DepartmentDaoTest.java
@@ -0,0 +1,52 @@
+package gradle_jdbc_study_teacher;
+
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import gradle_jdbc_study_teacher.dao.DepartmentDao;
+import gradle_jdbc_study_teacher.daoimpl.DepartmentDaoImpl;
+import gradle_jdbc_study_teacher.dto.Department;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class DepartmentDaoTest {
+	static final Logger log = LogManager.getLogger();
+	static DepartmentDao dao;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		dao = new DepartmentDaoImpl();
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		dao = null;
+	}
+
+	@Test
+	public void test01SelectDepartmentByAll() {
+		List<Department> lists = dao.selectDepartmentByAll();
+		for(Department d : lists) {
+			log.trace(d);
+		}
+		Assert.assertNotEquals(0, lists.size());
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
