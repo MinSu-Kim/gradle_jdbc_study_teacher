327ef89c84845292543d4de64cb90e365a9bc19f
영남인재교사 <net94.teacher@gmail.com>
2019-07-04 17:52:50 +0900

    테스트 제거

--- src/main/java/gradle_jdbc_study_teacher/ErpMain.java
+++ src/main/java/gradle_jdbc_study_teacher/ErpMain.java
@@ -6,9 +6,6 @@ import java.util.Date;
 public class ErpMain {
 
 	public static void main(String[] args) {
-		System.out.println(String.format("%tY", new Date()));
-		String str = String.format("E%s%03d", String.format("%tY", new Date()).substring(1), 1);
-		System.out.println(str);
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
